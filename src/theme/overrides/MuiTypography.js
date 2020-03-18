4fb251de561ae5fcef0e8843c6cc0a6a8c81db27
aamree <adilet90@mail.ru>
2020-03-18 11:47:10 +0600

    start project

---
+++ src/theme/overrides/MuiTypography.js
@@ -0,0 +1,14 @@
+// import TTNormsMediumTtf from 'fonts/TTNormsMedium.ttf';
+
+// const TTNormsMedium = {
+//   fontFamily: 'TT Norms Medium',
+//   fontWeight: 500,
+//   fontStyle: 'normal',
+//   src: `url(${TTNormsMediumTtf})`
+// };
+
+export default {
+  gutterBottom: {
+    marginBottom: 8
+  }
+};
